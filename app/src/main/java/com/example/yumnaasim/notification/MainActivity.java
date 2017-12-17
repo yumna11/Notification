afe8335ae4aca90c9caa551f55edbe590dbf75a5
YumnaAsim <yumnaasim11@gmail.com>
2017-12-17 15:52:52 +0500

    ringtone alert on notifying user

--- app/src/main/java/com/example/yumnaasim/notification/MainActivity.java
+++ app/src/main/java/com/example/yumnaasim/notification/MainActivity.java
@@ -5,10 +5,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.provider.Settings;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = new Intent(MainActivity.this, Main2Activity.class);
         notifyBuilder.setContentIntent(PendingIntent.getActivity(MainActivity.this,0,intent,0));
-        notifyBuilder.setContentTitle("New update available!");
-        notifyBuilder.setContentText("Install new update");
         notifyBuilder.setAutoCancel(true);
+        notifyBuilder.setVibrate(new long[]{0, 2000, 2000, 6000, 6000});
+        notifyBuilder.setLights(Color.RED,1000,1000);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        notifyBuilder.setSound(alarmSound, AudioManager.STREAM_NOTIFICATION);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.remote);
+        notifyBuilder.setContent(remoteViews);
 
         Notification notification = notifyBuilder.build();
         notificationManager.notify(NOTIFY_1,notification);
-
     }
 }
